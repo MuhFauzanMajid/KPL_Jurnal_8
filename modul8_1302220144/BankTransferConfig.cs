1755b7acddf58eb56b3644a1345909a178ef7e6d
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-04-18 10:59:09 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302220144/BankTransferConfig.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302220144
+{
+     class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<string> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+    }
+
+     class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+     class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+
+     class TestConfig
+    {
+
+        public BankTransferConfig config;
+
+        private const string file_path = "C:\\Kuliah\\Coding\\C#\\modul8_1302220144\\bank_transfer_config.json";
+
+        public TestConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+        public void ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(file_path);
+            this.config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+        }
+
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String text = JsonSerializer.Serialize(config);
+            File.WriteAllText(file_path, text);
+        }
+
+        public void SetDefault()
+        {
+            config = new BankTransferConfig();
+            Transfer transfer = new Transfer();
+            Confirmation confirmation = new Confirmation(); 
+
+            transfer.threshold = 25000000;
+            transfer.low_fee = 6500;
+            transfer.high_fee = 15000;
+
+            confirmation.en = "yes";
+            confirmation.id = "ya";
+            
+            config.lang = "en";
+            config.transfer = transfer;
+            config.methods = new List<String> { "RTO(real-time)", "SKN", "RTGS", "BI FAST" };
+            config.confirmation = confirmation;
+        }
+    }
+}
+
--- modul8_1302220144/Program.cs
+++ modul8_1302220144/Program.cs
@@ -1,7 +1,90 @@
-﻿internal class Program
+﻿using modul8_1302220144;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        TestConfig configtest = new TestConfig();
+
+        if(configtest.config.lang == "en")
+        {
+            Console.Write("Please insert the amount of money to transfer : ");
+        }
+        else
+        {
+            Console.Write("Masukkan jumlah uang yang akan di-transfer : ");
+        }
+        int amount = Convert.ToInt32(Console.ReadLine());
+
+        if(amount <= configtest.config.transfer.threshold)
+        {
+            if(configtest.config.lang == "en")
+            {
+                Console.WriteLine("Total amount = " + (amount+configtest.config.transfer.low_fee) );
+            }
+            else
+            {
+                Console.WriteLine("Total biaya = " + (amount + configtest.config.transfer.low_fee));
+            }
+        }else{
+            if (configtest.config.lang == "en")
+            {
+                Console.WriteLine("Total amount = " + (amount + configtest.config.transfer.high_fee));
+            }
+            else
+            {
+                Console.WriteLine("Total biaya = " + (amount + configtest.config.transfer.high_fee));
+            }
+        }
+        if(configtest.config.lang == "en")
+        {
+            Console.WriteLine("Select transfer method : ");
+        }
+        else
+        {
+            Console.WriteLine("pilih metode transfer : ");
+        }
+        
+        for(int i = 0; i < configtest.config.methods.Count; i++)
+        {
+            Console.WriteLine((i+1) + ". " + configtest.config.methods[i]);
+        }
+        
+        int method = Convert.ToInt32(Console.ReadLine());
+        
+        if(configtest.config.lang == "en")
+        {
+            Console.WriteLine("Please type " + configtest.config.confirmation.en + " to confirm the transaction");
+        }
+        else
+        {
+            Console.WriteLine("ketik " + configtest.config.confirmation.id + " untuk mengkonfirmasi transaksi");
+        }
+        
+        string confirmation = Console.ReadLine();
+
+        if (configtest.config.lang == "en")
+        {
+            if (confirmation == configtest.config.confirmation.en)
+            {
+                Console.WriteLine("Transaction is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transaction is canceled");
+            }
+        }
+        else if(configtest.config.lang == "id")
+        {
+            if (confirmation == configtest.config.confirmation.id)
+            {
+                Console.WriteLine("Proses transaksi berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transaksi dibatalkan");
+            }
+        }
+
     }
 }
